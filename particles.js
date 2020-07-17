ebf2938a890e3e6f09e94ce9830427dbd8b997b9
c0d3rMark <MarkM78@users.noreply.github.com>
2020-07-17 12:20:42 +1000

    configuing JSON file

---
+++ particles.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    particlesJS.load('particles-js', 'particles.json');
+    console.log(particlesJS);
+};
+
+// particlesJS.load('particles-js', 'particles.json', function() {
+//     console.log('particles.json loaded...');
+// } );
+
+
